17c835b7a41338fe7c2151367c20bea9a74b9a50
whimsy-Pan <panshaotao@gmail.com>
2015-07-26 16:30:46 -0500

    it is the homework

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These two functions are used to calculate the inverse of a matrix 
 
-## Write a short comment describing this function
-
+## makecacheMatrix function is used to store the inverse of a matix we calculated
 makeCacheMatrix <- function(x = matrix()) {
+      m <- NULL
+      set <- function(y) {
+            x <<- y
+            m <<- NULL
+             }
+      get <- function() x
+      setinverse <- function(solve) m <<- solve
+      getinverse <- function() m
+      list(set = set, get = get,
+           setinverse = setinverse,
+           getinverse = getinverse)
+      }
 
-}
 
-
-## Write a short comment describing this function
-
+## This main function is used to calculate the inverse of a matix if the invese has not been calculated
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+      m <- x$getinverse()
+      if(!is.null(m)) {
+            message("getting cached data")
+            return(m)
+            }
+      data <- x$get()
+      m <- solve(data, ...)
+      x$setinverse(m)
+      m
 }
+# the code below is to test the functions. First generate a matrix assigned to a, and then call the two functions and test the results.
+a <- matrix(data = c(1,2,3,0,1,4,5,6,0), nrow = 3, ncol = 3)
+matrixx <- makeCacheMatrix(a)
+matrixx$get()
+matrixx$getinverse()
+cacheSolve(matrixx)
+matrixx$getinverse()
+class(matrixx$get())
+class(matrixx$getinverse())
